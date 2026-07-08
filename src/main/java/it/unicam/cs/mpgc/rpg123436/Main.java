beab597386d5ec9bf78c2e4a1cc841cd1bc23316
Bigmagno7 <magnoni2004@gmail.com>
2026-07-08 13:29:02 +0200

    Configurato il main con javafx

--- src/main/java/it/unicam/cs/mpgc/rpg123436/Main.java
+++ src/main/java/it/unicam/cs/mpgc/rpg123436/Main.java
@@ -1,17 +1,41 @@
 package it.unicam.cs.mpgc.rpg123436;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+/**
+ * Classe principale del gioco. Estende 'Application' di JavaFX
+ * per poter usufruire della gestione delle finestre grafiche.
+ */
+public class Main extends Application {
+
+    // Il metodo 'start' è il punto di ingresso principale per le applicazioni JavaFX
+    @Override
+    public void start(Stage primaryStage) {
+        // Impostiamo il titolo che comparirà in alto sulla finestra del gioco
+        primaryStage.setTitle("Easy Dungeon RPG");
+
+        // Creiamo un semplice testo di benvenuto per verificare che la grafica funzioni
+        Text infoText = new Text("Benvenuto nel Dungeon! Il gioco sta per iniziare...");
+
+        // Lo StackPane è un contenitore che posiziona gli elementi al centro (in questo caso il testo)
+        StackPane root = new StackPane(infoText);
+
+        // Creiamo la scena del gioco impostando le dimensioni della finestra (800 pixel di larghezza, 600 di altezza)
+        Scene scene = new Scene(root, 800, 600);
+
+        // Colleghiamo la scena alla finestra principale (Stage)
+        primaryStage.setScene(scene);
+
+        // Mostriamo la finestra a schermo
+        primaryStage.show();
+    }
+
+    // Il metodo main classico che fa partire l'applicazione JavaFX
+    public static void main(String[] args) {
+        launch(args); // Lancia l'applicazione JavaFX chiamando il metodo start
     }
 }
