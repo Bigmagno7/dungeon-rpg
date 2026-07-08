2baab4ae4b4a53172708e27e4f1d0b7485176657
Bigmagno7 <magnoni2004@gmail.com>
2026-07-08 13:32:01 +0200

    Configurato il main con javafx

---
+++ src/main/java/it/unicam/cs/mpgc/rpg123436/Launcher.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.mpgc.rpg123436;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Avvia il vero Main passando da una classe normale senza extends Application
+        Main.main(args);
+    }
+}
